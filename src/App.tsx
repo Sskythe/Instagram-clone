8a73191aaabd49d83434e2f1676d7fcdcd60495c
Sskythe <kumar.saroj.1703s@gmail.com>
2023-11-30 15:53:18 +0530

    Base setup done defined the folder structure added the routing feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom';
 import './globals.css'
+import SignInForm from './_auth/Forms/SignInForm';
+import SignUpForm from './_auth/Forms/SignUpForm';
+import Home from './_root/Pages/Home';
+import AuthLayout from './_auth/AuthLayout';
+import PageLayout from './_root/PageLayout';
 const App = () => {
   return (
-    <div><h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1></div>
+
+    <main className='flex h-screen'>
+      <h1> Landing page</h1>
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SignInForm />} />
+          <Route path='/sign-up' element={<SignUpForm />} />
+        </Route>
+
+
+        {/* private route i.e. after sign up */}
+        <Route element={<PageLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+
+
+      </Routes>
+    </main>
+
   )
 }
 
